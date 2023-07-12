cd1dd3d57939710cdc02e41af463c668f999e60e
Net <Net@NetKBs>
2023-07-12 13:48:51 -0400

    PanelCitas: Botón agregar con ventana emergente hecho; Botón actualizar para reenlistar las citas; enlistamiento de citas en la tabla

--- src/views/panelCitas.java
+++ src/views/panelCitas.java
@@ -8,6 +8,12 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import ClasesGenerales.Consultorio;
+import ClasesGenerales.Cita;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Net
@@ -19,6 +25,13 @@ public class panelCitas extends javax.swing.JPanel {
      */
     public panelCitas() {
         initComponents();
+        // Obtener el modelo de datos de la tabla
+        /* DefaultTableModel model = (DefaultTableModel) tabla.getModel();
+         Object[] row = {234, "asdf", "sfd","asdf","asdf", "asdf"};
+            model.addRow(row);
+        
+         */
+        updateTable();
     }
 
     /**
@@ -33,25 +46,35 @@ public class panelCitas extends javax.swing.JPanel {
         jPanel1 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        tabla = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
 
         jPanel2.setBackground(new java.awt.Color(204, 204, 255));
 
-        jTable1.setBackground(new java.awt.Color(204, 204, 255));
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        tabla.setBackground(new java.awt.Color(204, 204, 255));
+        tabla.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null}
+
             },
             new String [] {
-                "ID", "Horario", "Motivo", "Paciente", "Correo", "Teléfono"
+                "ID", "Hora", "Motivo", "Paciente", "Correo", "Teléfono"
             }
-        ));
-        jScrollPane1.setViewportView(jTable1);
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, true, true, true, true, true
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        tabla.setColumnSelectionAllowed(true);
+        tabla.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        tabla.getTableHeader().setReorderingAllowed(false);
+        jScrollPane1.setViewportView(tabla);
+        tabla.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
 
         jButton1.setBackground(new java.awt.Color(102, 102, 255));
         jButton1.setFont(new java.awt.Font("Segoe UI", 1, 15)); // NOI18N
@@ -73,26 +96,39 @@ public class panelCitas extends javax.swing.JPanel {
             }
         });
 
+        jButton4.setBackground(new java.awt.Color(102, 102, 255));
+        jButton4.setFont(new java.awt.Font("Segoe UI", 1, 15)); // NOI18N
+        jButton4.setForeground(new java.awt.Color(255, 255, 255));
+        jButton4.setText("ACTUALIZAR");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 865, Short.MAX_VALUE)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addContainerGap(274, Short.MAX_VALUE)
+                .addGap(88, 88, 88)
                 .addComponent(jButton3)
-                .addGap(106, 106, 106)
+                .addGap(155, 155, 155)
+                .addComponent(jButton4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButton1)
-                .addGap(267, 267, 267))
+                .addGap(127, 127, 127))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addGap(0, 112, Short.MAX_VALUE)
+                .addGap(0, 120, Short.MAX_VALUE)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton3)
-                    .addComponent(jButton1))
-                .addGap(36, 36, 36)
+                    .addComponent(jButton1)
+                    .addComponent(jButton4))
+                .addGap(28, 28, 28)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 338, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
@@ -135,19 +171,57 @@ public class panelCitas extends javax.swing.JPanel {
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
-       
+
         crearCitass c = new crearCitass();
-        new MyPopup(c).setVisible(true); 
+        new MyPopup(c).setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+        updateTable();
+        // Revalidate and repaint the table    
+        tabla.revalidate();
+        tabla.repaint();
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     // La ventana emergente
     class MyPopup extends JFrame {
 
         public MyPopup(JPanel externalPanel) {
+
             setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-            Dimension preferredSize = externalPanel.getPreferredSize();        
-          setSize(preferredSize.width, preferredSize.height);  
-            getContentPane().add(externalPanel); 
+            Dimension preferredSize = externalPanel.getPreferredSize();
+            setSize(preferredSize.width, preferredSize.height);
+            getContentPane().add(externalPanel);
+        }
+    }
+
+    // Actualiar y cargar tabla
+    public void updateTable() {
+
+        // Obtenemos datos de las citas
+        ArrayList<Cita> citas = Consultorio.instance.getCitas_activas();
+
+        // Obtener el modelo de datos de la tabla
+        DefaultTableModel model = (DefaultTableModel) tabla.getModel();
+
+        // Limpiar la tabla existente
+        model.setRowCount(0);
+
+        // Agregar nuevas filas 
+        for (Cita cita : citas) {
+            // Obtener los datos de la fila
+            int id = cita.getId();
+            String horario = cita.getHorario();
+            String motivo = cita.getMotivo();
+            String paciente = cita.getPaciente().getNombres() + " " + cita.getPaciente().getApellidos();
+            String correo = cita.getPaciente().getCorreo();
+            String telefono = cita.getPaciente().getTlfno();
+
+            // Agregar la fila al modelo de datos
+            Object[] row = {id, horario, motivo, paciente, correo, telefono};
+            model.addRow(row);
+  
         }
     }
 
@@ -155,9 +229,10 @@ public class panelCitas extends javax.swing.JPanel {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
+    private javax.swing.JTable tabla;
     // End of variables declaration//GEN-END:variables
 }
